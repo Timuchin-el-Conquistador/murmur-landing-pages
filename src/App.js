ec3457bb554283b7f9ab52bc958b24b21572f2fa
Chingiz <hemidovcingiz183@gmail.com>
2022-02-04 00:39:14 +0400

    create-react-app streetiq first and second landing pages

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './assets/scss/theme.scss'
+import React from 'react';
+
+import StreetIQ from './pages/street-iq';
+
+
+function App() {
+  return (
+         <React.Fragment>
+             <StreetIQ/>
+         </React.Fragment>
+  );
+}
+
+export default App;
